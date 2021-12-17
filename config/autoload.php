31b0cca4a13bcd8b89ca09441ff20e5630b19e71
Daniel Silva de Sousa <diielsilvasousa@gmail.com>
2021-12-16 23:18:15 -0300

    Incluindo funcionalidade para o autoload carregar serviços

--- config/autoload.php
+++ config/autoload.php
@@ -2,7 +2,11 @@
 
 spl_autoload_register(function ($className) {
     try {
-        require_once $_SERVER["DOCUMENT_ROOT"] . "/project-manager/models/" . $className . ".php";
+        if (file_exists($_SERVER["DOCUMENT_ROOT"] . "/project-manager/models/" . $className . ".php")) {
+            require_once $_SERVER["DOCUMENT_ROOT"] . "/project-manager/models/" . $className . ".php";
+        } else if (file_exists($_SERVER["DOCUMENT_ROOT"] . "/project-manager/services/" . $className . ".php")) {
+            require_once $_SERVER["DOCUMENT_ROOT"] . "/project-manager/services/" . $className . ".php";
+        }
     } catch (Exception $error) {
         echo $error->getMessage();
         die;
